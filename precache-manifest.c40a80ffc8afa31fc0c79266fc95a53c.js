78eb8d0e08eb0b7da59cabb85b460053fa50ddaf
Alfred Fung <alfred@fung-family.ca>
2018-11-26 22:06:51 -0800

    Updates

---
+++ precache-manifest.c40a80ffc8afa31fc0c79266fc95a53c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebf98fc4a5e74e481af8",
+    "url": "/static/js/main.ebf98fc4.chunk.js"
+  },
+  {
+    "revision": "ee6afb1f96b00648d6e0",
+    "url": "/static/js/1.ee6afb1f.chunk.js"
+  },
+  {
+    "revision": "4691843e63eaa95b7c84600da88774be",
+    "url": "/index.html"
+  }
+];
