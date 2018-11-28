c0f108248cd4159005d53c966747017cbe79ea05
Alfred Fung <alfred@fung-family.ca>
2018-11-27 21:39:03 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AEHelper/precache-manifest.c5972fffcdd2a8b760210738c34f9893.js"
+  "/AEHelper/precache-manifest.d325d59778bc74b65b803e350d47a520.js"
 );
 
 workbox.clientsClaim();
